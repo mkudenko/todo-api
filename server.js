f1354752a0d678b5488679a51aa69bbad7ae6b65
Michael Kudenko <mkudenko@gmail.com>
2017-01-11 11:18:37 +1300

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.find(todos, {id: todoId});
     if (typeof matchedTodo === 'undefined') {
-        res.status(404).send();
+        return res.status(404).send();
     } else {
         res.json(matchedTodo);
     }
@@ -45,11 +45,37 @@ app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.find(todos, {id: todoId});
     if (!matchedTodo) {
-        res.status(404).json({"error": "no todo found with that id"});
+        return res.status(404).json({"error": "no todo found with that id"});
     } else {
         todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
     }
+});
+
+app.put('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.find(todos, {id: todoId});
+    var body = _.pick(req.body, ['description', 'completed']);
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).json({"error": "no todo found with that id"});
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description.trim();
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.assign(matchedTodo, validAttributes);
+
+    res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
