413f7238436d0eb8d39d42cb7fc6d9492c098a10
Michael Kudenko <mkudenko@gmail.com>
2017-01-11 12:09:47 +1300

    Add ?completed filter

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ app.get('/', function (req, res) {
 });
 
 app.get('/todos', function (req, res) {
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.filter(filteredTodos, {completed: true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.filter(filteredTodos, {completed: false});
+    }
+
+    res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function (req, res) {
