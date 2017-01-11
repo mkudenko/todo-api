74624c5aeee0a29a093c62e4d02d281498e50706
Michael Kudenko <mkudenko@gmail.com>
2017-01-11 13:18:29 +1300

    Add sequelize test

---
+++ playground/variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+//     name: 'Andrew',
+//     age: 21
+// };
+//
+// function updatePerson(obj) {
+//     // obj = {
+//     //     name: 'Andrew',
+//     //     age: 24
+//     // };
+//     obj.age = 24;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+var animals = ['dog', 'cat'];
+
+function addAnimal(list) {
+    // list = ['dog', 'cat', 'parrot'];
+    list.push('parrot');
+    debugger;
+}
+
+addAnimal(animals);
+console.log(animals);
